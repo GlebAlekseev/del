3ccf5872db31f33562648b15f52e81cefb355fc9
GlebAlekseev <glebalekseevjk@gmail.com>
2024-02-18 13:33:12 +0300

    added toolbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
 }
 
 dependencies {
-    implementation("androidx.navigation:navigation-fragment:2.7.7")
-    implementation("androidx.navigation:navigation-ui:2.7.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
--- app/src/main/java/com/glebalekseev/lab/activity/MainActivity.kt
+++ app/src/main/java/com/glebalekseev/lab/activity/MainActivity.kt
@@ -3,18 +3,54 @@ package com.glebalekseev.lab.activity
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.glebalekseev.lab.R
-import com.glebalekseev.lab.controller.NoteController
+
 
 class MainActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "MainActivity >> "
     }
 
+    private val navController: NavController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.mainNavHostFragment) as NavHostFragment).navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupToolbar()
+        setupActionBarWithNavController()
+        setupDestinationChangeListener()
+
         Log.i(TAG, "onCreate")
+    }
+
+    private fun setupToolbar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    private fun setupActionBarWithNavController() {
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    private fun setupDestinationChangeListener() {
+        navController.addOnDestinationChangedListener { _, destination: NavDestination, _ ->
+            when (destination.id) {
+                R.id.mainFragment -> supportActionBar?.title = getString(R.string.note_list)
+                R.id.noteDetailFragment -> supportActionBar?.title =
+                    getString(R.string.note_description)
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
     override fun onStart() {
